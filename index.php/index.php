a69f9d304b249b256dfcb30f51c43ac2cadead94
salihstepup <mohammedsalihred@gmail.com>
2020-06-24 21:44:28 +0530

    first

---
+++ index.php/index.php
@@ -0,0 +1,14 @@
+<?php
+$n1=1;
+$n2=2;
+
+$r=$n1+$n2;
+echo("sum is $r");
+$r=$n1-$n2;
+echo("subtraction is $r");
+$r=$n1*$n2;
+echo(" multiplication is $r");
+$r=$n1 / $n2;
+echo("division is $r");
+
+?>
